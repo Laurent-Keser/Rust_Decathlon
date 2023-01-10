2356caeaab98f458a4c03b32f7b5d92219b4551d
Laurent-Keser <lau.keser@gmail.com>
2023-01-10 15:09:04 +0100

    adding the file with all the cities informations

--- Wheater_Forecast/src/main.rs
+++ Wheater_Forecast/src/main.rs
@@ -1,15 +1,25 @@
-//importing the io lib 
+//importing the io lib
 //use std::io;
 
 fn main() {
-
     //creating a vector (list) of somme cities. If no datas a providen you can : let v: Vec<i32> = Vec::new();
-    let list_cities = vec!["Brussels","Mons","Liège","Eupen","Namur","Braine-le-Comte","Soignies","Charleroi","Dinant","Nivelles"];
-    
+    let list_cities = vec![
+        "Brussels",
+        "Mons",
+        "Liège",
+        "Eupen",
+        "Namur",
+        "Braine-le-Comte",
+        "Soignies",
+        "Charleroi",
+        "Dinant",
+        "Nivelles",
+    ];
+
     println!("My favourite cities are : ");
-    for i in &list_cities{
+
+    //for each element in the vector, print the value
+    for i in &list_cities {
         println!("{i}");
     }
-    
-    
 }
