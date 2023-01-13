81499d6f91e4c314febdd4a90af753757728e6fd
Laurent-Keser <lau.keser@gmail.com>
2023-01-13 20:56:14 +0100

    chore: cleaning up

--- weather_forecast/src/main.rs
+++ weather_forecast/src/main.rs
@@ -18,49 +18,64 @@ use std::io;
  */
 #[tokio::main]
 async fn main() {
-    // task1().await;
-    task2().await;
+    display_menu().await;
 }
 
-async fn task1() {
-    // Getting API_KEY from environment file
-    dotenv().ok();
-
-    let api_key: String = env::var("API_KEY").unwrap();
-    let list_cities: Vec<&str> = forecast::get_cities();
-
-    for city in &list_cities {
-        let client = Client::new(); // HTTP client
-        let forecast_result = forecast::get_weather_data(&client, &api_key, city)
-            .await
-            .unwrap();
-
-        forecast::print_forecast(forecast_result);
-    }
-}
-
-async fn task2() {
-    // Displaying list of cities
-    println!("\n[List of cities]");
-    let list_cities: Vec<&str> = forecast::get_cities();
-
-    for (index, city) in list_cities.iter().enumerate() {
-        println!("  {} - {}", index + 1, city);
-    }
-
-    // Reading user input and retrieving city
-    let selected_city = read_city_input(list_cities);
-    println!("You selected: {}\n", selected_city);
-
-    // Displaying weather forecast
+/**
+ * Displays the selection menu of this project.
+ */
+async fn display_menu() {
+    // Setup API key and HTTP client
     dotenv().ok();
     let api_key: String = env::var("API_KEY").unwrap();
     let client = Client::new(); // HTTP client
-    let forecast_result = forecast::get_weather_data(&client, &api_key, &selected_city)
-        .await
-        .unwrap();
 
-    forecast::print_forecast(forecast_result);
+    // Displaying message
+    println!("\n[*** RUST ASSESSMENT - DECATHLON ***]");
+    println!("\nSelect an option: ");
+
+    let options = vec![
+        "Display the daily weather forecast of 10 Belgian cities",
+        "Display the daily weather forecast of a city from the list",
+        "Display the daily weather forecast from another city",
+    ];
+
+    for (index, option) in options.iter().enumerate() {
+        println!("  {} - {}", index + 1, option);
+    }
+
+    let mut input = String::new();
+    loop {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("");
+        let selected_index: Result<usize, _> = input.trim().parse();
+
+        match selected_index {
+            Ok(index) => {
+                if index > 0 && index <= options.len() {
+                    match index - 1 {
+                        0 => task1(&client, &api_key).await,
+                        1 => task2(&client, &api_key).await,
+                        2 => println!("WIP"),
+                        _ => task1(&client, &api_key).await,
+                    }
+                    break;
+                } else {
+                    println!(
+                        "Please enter a valid number between 1 and {} : ",
+                        options.len()
+                    );
+                }
+            }
+            Err(_) => {
+                println!(
+                    "Please enter a valid number between 1 and {} : ",
+                    options.len()
+                );
+            }
+        }
+    }
 }
 
 /**
@@ -73,6 +88,7 @@ fn read_city_input(list_cities: Vec<&str>) -> String {
     loop {
         input.clear();
         io::stdin().read_line(&mut input).unwrap();
+        println!("");
         let selected_index: Result<usize, _> = input.trim().parse();
 
         match selected_index {
@@ -94,4 +110,44 @@ fn read_city_input(list_cities: Vec<&str>) -> String {
             }
         }
     }
+}
+
+/**
+ * Displays the weather forecast for all 10 Belgian cities.
+ */
+async fn task1(client: &Client, api_key: &str) {
+    let list_cities: Vec<&str> = forecast::get_cities();
+
+    for city in &list_cities {
+        let forecast_result = forecast::get_weather_data(&client, &api_key, city)
+            .await
+            .unwrap();
+        forecast::print_forecast(forecast_result);
+    }
+}
+
+/**
+ * Displays a list of city to choose from, to then display its weather forecast
+ */
+async fn task2(client: &Client, api_key: &str) {
+    // Displaying list of cities
+    println!("\n[List of cities]");
+    let list_cities: Vec<&str> = forecast::get_cities();
+
+    for (index, city) in list_cities.iter().enumerate() {
+        println!("  {} - {}", index + 1, city);
+    }
+
+    // Reading user input and retrieving city
+    let selected_city = read_city_input(list_cities);
+    println!("You selected: {}\n", selected_city);
+
+    // Displaying weather forecast
+    dotenv().ok();
+
+    let forecast_result = forecast::get_weather_data(&client, &api_key, &selected_city)
+        .await
+        .unwrap();
+
+    forecast::print_forecast(forecast_result);
 }
